50f80407eb8fd3f72b45e7a5bd07934014e78cfb
Deepak Narayanan <deepakn94@gmail.com>
2015-12-29 21:10:17 +0530

    Small fix to server.sh

--- bin/server.sh
+++ bin/server.sh
@@ -1,2 +1,2 @@
 ulimit -u unlimited
-java -cp "src/main/resources/:target/classes:target/lib/*:target/dependency/*" macrobase.MacroBase server config/macrobase.yaml
+java -cp "src/main/resources/:target/classes:target/lib/*:target/dependency/*" macrobase.MacroBase server conf/macrobase.yaml
