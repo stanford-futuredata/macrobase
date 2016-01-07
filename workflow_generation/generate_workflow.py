bd0ec28ba7d3fb429b3ccd2f49b2e72317f75ac9
Deepak Narayanan <deepakn94@gmail.com>
2016-01-07 15:41:25 -0800

    Added ability to toggle between batch and streaming jobs

--- workflow_generation/generate_workflow.py
+++ workflow_generation/generate_workflow.py
@@ -1,25 +1,36 @@
 import os
 
 testing_dir = "workflows"
-template_conf_file = "batch_template.conf"
+batch_template_conf_file = "batch_template.conf"
+streaming_template_conf_file = "streaming_template.conf"
 all_config_parameters = \
 [
   {
+    "isBatchJob": True,
     "taskName": "testTasks",
-    "targetAttributes": ["userid"],
-    "targetLowMetrics": ["data_count_minutes"],
-    "targetHighMetrics": [],
+    "targetAttributes": ["device_id", "state", "model", "firmware_version"],
+    "targetLowMetrics": [],
+    "targetHighMetrics": ["power_drain"],
 
-    "baseQuery": "SELECT * FROM mapmatch_history H, sf_datasets D WHERE H.dataset_id = D.id LIMIT 100000;"
+    "baseQuery": "SELECT * FROM sensor_data;"
   },
   {
+    "isBatchJob": False,
     "taskName": "testTasks2",
-    "targetAttributes": ["userid"],
-    "targetLowMetrics": ["data_count_minutes"],
-    "targetHighMetrics": [],
+    "targetAttributes": ["device_id", "state", "model", "firmware_version"],
+    "targetLowMetrics": [],
+    "targetHighMetrics": ["power_drain"],
 
-    "baseQuery": "SELECT * FROM mapmatch_history H, sf_datasets D WHERE H.dataset_id = D.id LIMIT 1000;"
-  },
+    "inputReservoirSize": 10000,
+    "scoreReservoirSize": 10000,
+    "inlierItemSummarySize": 1000,
+    "outlierItemSummarySize": 10000,
+    "summaryRefreshPeriod": 100000,
+    "modelRefreshPeriod": 10000,
+    "warmupCount": 1000,
+
+    "baseQuery": "SELECT * FROM sensor_data;"
+  }
 ]
 
 def process_config_parameters(config_parameters):
@@ -28,6 +39,7 @@ def process_config_parameters(config_parameters):
       config_parameters[config_parameter_type] = ", ".join([str(para) for para in config_parameters[config_parameter_type]])
 
 def create_config_file(config_parameters, conf_file):
+  template_conf_file = batch_template_conf_file if config_parameters["isBatchJob"] else streaming_template_conf_file
   template_conf_contents = open(template_conf_file, 'r').read()
   conf_contents = template_conf_contents % config_parameters
   with open(conf_file, 'w') as f:
@@ -38,6 +50,8 @@ if __name__ == '__main__':
     sub_dir = os.path.join(os.getcwd(), testing_dir, config_parameters["taskName"])
     os.system("mkdir -p %s" % sub_dir)
     process_config_parameters(config_parameters)
-    conf_file = os.path.join(sub_dir, "batch.conf")
+    conf_file = "batch.conf" if config_parameters["isBatchJob"] else "streaming.conf"
+    conf_file = os.path.join(sub_dir, conf_file)
     create_config_file(config_parameters, conf_file)
-    os.system("cd ..; java ${JAVA_OPTS} -cp \"src/main/resources/:target/classes:target/lib/*:target/dependency/*\" macrobase.MacroBase batch %s" % conf_file) 
+    cmd = "batch" if config_parameters["isBatchJob"] else "streaming"
+    os.system("cd ..; java ${JAVA_OPTS} -cp \"src/main/resources/:target/classes:target/lib/*:target/dependency/*\" macrobase.MacroBase %s %s" % (cmd, conf_file))
