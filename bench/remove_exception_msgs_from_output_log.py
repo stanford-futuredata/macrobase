87d8e2eed9429132b9e327c66486831f7bbe4999
Deepak Narayanan <deepakn94@gmail.com>
2016-01-22 14:32:42 -0800

    Script to remove exceptions from results file

---
+++ bench/remove_exception_msgs_from_output_log.py
@@ -0,0 +1,20 @@
+import sys
+
+if (len(sys.argv) < 2):
+  print "Incorrect usage: python %s <results_file>" % sys.argv[0]
+  exit(-1)
+
+with open(sys.argv[1], 'r') as f:
+  lines = f.read().split('\n')
+  i = 0
+  while (i < len(lines)):
+    line = lines[i]
+    if "Exception" in line:
+      index = line.find("Exception")
+      line = line[:index]
+      i+=1
+      while (lines[i].startswith('\t') or "Exception" in lines[i]):
+        i+=1
+      line += lines[i]
+    print line
+    i+=1
