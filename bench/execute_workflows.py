c1c8d7bee229ad49f5e8700efb8b72c22e2d7cee
Deepak Narayanan <deepakn94@gmail.com>
2016-01-22 13:39:27 -0800

    Move sweeping parameters out of main execute_workflows script

--- bench/execute_workflows.py
+++ bench/execute_workflows.py
@@ -1,6 +1,7 @@
 import os
 
 from config_parameters import all_config_parameters
+from sweeping_parameters import sweeping_parameters
 from time import strftime
 
 testing_dir = "workflows"
@@ -33,21 +34,6 @@ default_args = {
 
   "alphaMCD": 0.5,
   "stoppingDeltaMCD": 0.001
-}
-
-sweeping_parameters = {
-  "alphaMCD": [1.0, 0.5, 0.05],
-  "stoppingDeltaMCD": [1.0, 0.01, 0.0001],
-  "inputReservoirSize": [1, 100, 500, 10000, 100000],
-  "scoreReservoirSize": [1, 100, 500, 10000, 100000],
-  "summaryRefreshPeriod": [10, 1000, 100000],
-  "modelRefreshPeriod": [10, 1000, 100000],
-
-  "minSupport": [1.0, 0.5, 0.05, 0.001],
-  "minInlierRatio": [1.0, 2.0, 5.0, 50.0],
-  "targetPercentile": [0.5, 0.9, 0.999],
-
-  "warmupCount": [1, 50, 10000, 100000]
 }
 
 def process_config_parameters(config_parameters):
---
+++ bench/sweeping_parameters.py
@@ -0,0 +1,14 @@
+sweeping_parameters = {
+  "alphaMCD": [1.0, 0.5, 0.05],
+  "stoppingDeltaMCD": [1.0, 0.01, 0.0001],
+  "inputReservoirSize": [1, 100, 500, 10000, 100000],
+  "scoreReservoirSize": [1, 100, 500, 10000, 100000],
+  "summaryRefreshPeriod": [10, 1000, 100000],
+  "modelRefreshPeriod": [10, 1000, 100000],
+
+  "minSupport": [1.0, 0.5, 0.05, 0.001],
+  "minInlierRatio": [1.0, 2.0, 5.0, 50.0],
+  "targetPercentile": [0.5, 0.9, 0.999],
+
+  "warmupCount": [1, 50, 10000, 100000]
+}
