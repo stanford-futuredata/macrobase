572aa3ee0c4d75f1ef842fefd44672898a288ebd
Peter Bailis <pbailis@gmail.com>
2016-02-15 22:34:03 -0800

    Add test for ExactCount.

---
+++ src/test/java/macrobase/summary/count/ExactCountTest.java
@@ -0,0 +1,37 @@
+package macrobase.summary.count;
+
+import com.google.common.collect.Lists;
+import macrobase.analysis.summary.count.ExactCount;
+import macrobase.analysis.summary.result.DatumWithScore;
+import macrobase.datamodel.Datum;
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExactCountTest {
+    @Test
+    public void testCount() {
+        ExactCount ec = new ExactCount();
+        HashMap<Integer, Integer> truth = new HashMap<>();
+
+        List<DatumWithScore> dws = new ArrayList<>();
+        for(int i = 0; i < 100; ++i) {
+            for(int j = 0; j < i; ++j) {
+                dws.add(new DatumWithScore(new Datum(Lists.newArrayList(i), new ArrayRealVector()), 0));
+                truth.compute(i, (k, v) -> v == null ? 1 : v + 1);
+            }
+        }
+
+        ec.count(dws);
+
+        for(Map.Entry<Integer, Double> cnt : ec.getCounts().entrySet()) {
+            assertEquals(truth.get(cnt.getKey()), cnt.getValue(), 1e-10);
+        }
+    }
+}
