00eb42e373b048313cc12383b45b40ffbc01adf9
Peter Bailis <pbailis@gmail.com>
2016-06-13 21:00:21 -0700

    Shuffle streaming data for non-time-series analysis.

--- src/main/java/macrobase/analysis/pipeline/BasicOneShotEWStreamingPipeline.java
+++ src/main/java/macrobase/analysis/pipeline/BasicOneShotEWStreamingPipeline.java
@@ -22,6 +22,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -45,6 +47,7 @@ public class BasicOneShotEWStreamingPipeline extends BasePipeline {
         FeatureTransform normalizer = new LinearMetricNormalizer();
         normalizer.consume(ingester.getStream().drain());
         List<Datum> data = normalizer.getStream().drain();
+        Collections.shuffle(data);
         System.gc();
         final long loadMs = sw.elapsed(TimeUnit.MILLISECONDS);
 
