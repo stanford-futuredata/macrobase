1b9c80b182fd3e68f1640269013ba0e6a73de3aa
Peter Bailis <pbailis@gmail.com>
2016-06-23 13:50:49 -0700

    Test chunked I/O for caching ingester.

--- src/test/java/macrobase/ingest/CachingSQLIngesterTest.java
+++ src/test/java/macrobase/ingest/CachingSQLIngesterTest.java
@@ -197,4 +197,156 @@ public class CachingSQLIngesterTest {
 
         assertTrue(firstVals2.isEmpty());
     }
+
+
+    @Test
+    public void testChunkedIO() throws Exception {
+        JDBCMockObjectFactory factory = new JDBCMockObjectFactory();
+        factory.registerMockDriver();
+        MockConnection connection = factory.getMockConnection();
+        StatementResultSetHandler statementHandler =
+                connection.getStatementResultSetHandler();
+        MockResultSet result = statementHandler.createResultSet();
+
+        MockResultSetMetaData metaData = new MockResultSetMetaData();
+
+        List<Integer> fakeData = new ArrayList<>();
+
+        final int NUM_ROWS = 1000;
+        final int NUM_ATTRS = 5;
+        final int NUM_HIGH = 2;
+        final int NUM_LOW = 3;
+        final int NUM_AUXILIARY = 1;
+
+        final int DIMENSION = NUM_ATTRS + NUM_HIGH + NUM_LOW + NUM_AUXILIARY;
+
+        Integer val = 1;
+        Set<String> firstVals = new HashSet<>();
+        for(int rno = 0; rno < NUM_ROWS; ++rno) {
+            List<Object> rowString = new ArrayList<>();
+            firstVals.add(val.toString());
+            for(Integer i = 0; i < DIMENSION; ++i) {
+                rowString.add(val.toString());
+                val++;
+            }
+            result.addRow(rowString);
+        }
+
+        int column = 1;
+        List<String> attributes = new ArrayList<>();
+        for(int i = 0; i < NUM_ATTRS; ++i) {
+            String attrName = String.format("attr%d", i);
+            metaData.setColumnName(column, attrName);
+            attributes.add(attrName);
+            column++;
+        }
+
+        List<String> lowMetrics = new ArrayList<>();
+        for(int i = 0; i < NUM_LOW; ++i) {
+            String metricName = String.format("lowMetric%d", i);
+            metaData.setColumnName(column, metricName);
+            lowMetrics.add(metricName);
+            column++;
+        }
+
+        List<String> highMetrics = new ArrayList<>();
+        for(int i = 0; i < NUM_HIGH; ++i) {
+            String metricName = String.format("highMetric%d", i);
+            metaData.setColumnName(column, metricName);
+            highMetrics.add(metricName);
+            column++;
+        }
+
+        List<String> auxiliaryAttributes = new ArrayList<>();
+        for(int i = 0; i < NUM_AUXILIARY; ++i) {
+            String metricName = String.format("auxiliary%d", i);
+            metaData.setColumnName(column, metricName);
+            auxiliaryAttributes.add(metricName);
+            column++;
+        }
+
+        Set<String> firstVals2 = Sets.newHashSet(firstVals);
+
+        metaData.setColumnCount(DIMENSION);
+
+        result.setResultSetMetaData(metaData);
+
+        statementHandler.prepareGlobalResultSet(result);
+
+        MacroBaseConf conf = new MacroBaseConf();
+        conf.set(MacroBaseConf.ATTRIBUTES, attributes);
+        conf.set(MacroBaseConf.LOW_METRICS, lowMetrics);
+        conf.set(MacroBaseConf.HIGH_METRICS, highMetrics);
+        conf.set(MacroBaseConf.AUXILIARY_ATTRIBUTES, auxiliaryAttributes);
+        conf.set(MacroBaseConf.DB_CACHE_DIR, folder.newFolder());
+
+        conf.set(MacroBaseConf.BASE_QUERY, "SELECT * FROM test;");
+        conf.set(MacroBaseConf.DB_CACHE_CHUNK_SIZE, 1);
+
+        DatumEncoder encoder = conf.getEncoder();
+        List<Datum> data = new DiskCachingIngester(conf, new TestSQLIngester(conf, connection)).getStream().drain();
+
+        for (Datum d : data) {
+            String firstValString = encoder.getAttribute(d.getAttributes().get(0)).getValue();
+            Integer curValInt = Integer.parseInt(firstValString);
+            assertTrue(firstVals.contains(firstValString));
+            firstVals.remove(firstValString);
+
+            column = 0;
+            for(int i = 0; i < NUM_ATTRS; ++i) {
+                assertEquals(curValInt,
+                             (Integer) Integer.parseInt(encoder.getAttribute(d.getAttributes().get(i)).getValue()));
+                curValInt++;
+                column++;
+            }
+
+            for(int i = 0; i < NUM_LOW; ++i) {
+                assertEquals(Math.pow(Math.max(curValInt, 0.1), -1), d.getMetrics().getEntry(i), 0);
+                curValInt++;
+            }
+
+            for(int i = NUM_LOW; i < NUM_LOW+NUM_HIGH; ++i) {
+                assertEquals(curValInt, d.getMetrics().getEntry(i), 0);
+                curValInt++;
+            }
+
+            assertEquals(curValInt, d.getAuxiliaries().getEntry(0), 0);
+        }
+
+        assertTrue(firstVals.isEmpty());
+
+        DatumEncoder encoder2 = conf.getEncoder();
+        SQLIngester t = new TestSQLIngester(conf, connection);
+        t.connect();
+        List<Datum> data2 = new DiskCachingIngester(conf, t).getStream().drain();
+
+        for (Datum d : data2) {
+            String firstValString = encoder2.getAttribute(d.getAttributes().get(0)).getValue();
+            Integer curValInt = Integer.parseInt(firstValString);
+            assertTrue(firstVals2.contains(firstValString));
+            firstVals2.remove(firstValString);
+
+            column = 0;
+            for(int i = 0; i < NUM_ATTRS; ++i) {
+                assertEquals(curValInt,
+                             (Integer) Integer.parseInt(encoder2.getAttribute(d.getAttributes().get(i)).getValue()));
+                curValInt++;
+                column++;
+            }
+
+            for(int i = 0; i < NUM_LOW; ++i) {
+                assertEquals(Math.pow(Math.max(curValInt, 0.1), -1), d.getMetrics().getEntry(i), 0);
+                curValInt++;
+            }
+
+            for(int i = NUM_LOW; i < NUM_LOW+NUM_HIGH; ++i) {
+                assertEquals(curValInt, d.getMetrics().getEntry(i), 0);
+                curValInt++;
+            }
+
+            assertEquals(curValInt, d.getAuxiliaries().getEntry(0), 0);
+        }
+
+        assertTrue(firstVals2.isEmpty());
+    }
 }
