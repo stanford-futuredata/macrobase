f2fee0e2511fc760af57dd09f6cdcb4e1f989e20
Peter Bailis <pbailis@gmail.com>
2016-07-02 19:50:18 -0700

    add are to hh compare

--- src/test/java/macrobase/bench/HeavyHittersComparePipeline.java
+++ src/test/java/macrobase/bench/HeavyHittersComparePipeline.java
@@ -1,6 +1,8 @@
 package macrobase.bench;
 
 import com.google.common.base.Stopwatch;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import macrobase.analysis.classify.BatchingPercentileClassifier;
 import macrobase.analysis.classify.DumpClassifier;
@@ -31,6 +33,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -67,6 +70,15 @@ public class HeavyHittersComparePipeline extends BasePipeline {
         return tsw.elapsed(TimeUnit.MICROSECONDS);
     }
 
+    private double compute_are(Map<Integer, Double> actual, Map<Integer, Double> appx) {
+        double ret = 0;
+        for(Map.Entry<Integer, Double> ae : actual.entrySet()) {
+            ret += ae.getValue()-appx.get(ae.getKey());
+        }
+
+        return ret / actual.size();
+    }
+
     private long bench_external_decay(List<Datum> data, ApproximateCount count, int refreshRate) {
         Stopwatch tsw = Stopwatch.createUnstarted();
         tsw.start();
@@ -99,38 +111,52 @@ public class HeavyHittersComparePipeline extends BasePipeline {
         Stopwatch tsw = Stopwatch.createUnstarted();
         tsw.start();
 
-        int[] refreshRates = {10000, 100000, 1000000};
-        int[] sizes = {100, 1000, 10000, 100000, 1000000};
+
+        int[] refreshRates = {100000, 10000, 1000000};
+        int[] sizes = {10, 100, 1000, 10000, 100000, 1000000};
         for (int rr : refreshRates) {
+            AmortizedMaintenanceCounter trueValue = new AmortizedMaintenanceCounter(1000000000);
+            bench_native_decay(data, trueValue, rr);
+
+            List<Double> trueVals = Lists.newArrayList(trueValue.getCounts().values());
+            Collections.sort(trueVals);
+            Collections.reverse(trueVals);
+
             for (int size : sizes) {
+                double minHHVal = trueVals.get(size);
+                Map<Integer, Double> trueHHs = Maps.newHashMap();
+                for(Map.Entry<Integer, Double> e : trueHHs.entrySet()) {
+                    if(e.getValue() > minHHVal) {
+                        trueHHs.put(e.getKey(), e.getValue());
+                    }
+                }
+
                 for (int i = 0; i < 5; ++i) {
                     SpaceSavingHeap ssh = new SpaceSavingHeap(size);
                     SpaceSavingList ssl = new SpaceSavingList(size);
                     AmortizedMaintenanceCounter fbsl = new AmortizedMaintenanceCounter(size);
 
                     log.debug("DATASIZE: {}", data.size());
-                    log.debug("SSH {} {} {}", size, rr, bench_native_decay(data, ssh, rr));
-                    log.debug("SSL {} {} {}", size, rr, bench_native_decay(data, ssl, rr));
-                    log.debug("FBSL {} {} {}", size, rr, bench_native_decay(data, fbsl, rr));
+                    log.debug("SSH {} {} {} {}", size, rr, bench_native_decay(data, ssh, rr), compute_are(trueHHs, ssh.getCounts()));
+                    log.debug("SSL {} {} {} {}", size, rr, bench_native_decay(data, ssl, rr), compute_are(trueHHs,
+                                                                                                          ssl.getCounts()));
+                    log.debug("FBSL {} {} {} {}", size, rr, bench_native_decay(data, fbsl, rr), compute_are(trueHHs,
+                                                                                                            fbsl.getCounts()));
                 }
-            }
-        }
 
-        for (int rr : refreshRates) {
-            for (int size : sizes) {
                 for (int i = 0; i < 5; ++i) {
                     SpaceSavingHeap ssh = new SpaceSavingHeap(size);
                     SpaceSavingList ssl = new SpaceSavingList(size);
                     AmortizedMaintenanceCounter fbsl = new AmortizedMaintenanceCounter(size);
 
                     log.debug("DATASIZE: {}", data.size());
-                    log.debug("SSH_EXT {} {} {}", size, rr, bench_external_decay(data, ssh, rr));
-                    log.debug("SSL_EXT {} {} {}", size, rr, bench_external_decay(data, ssl, rr));
-                    log.debug("FBSL_EXT {} {} {}", size, rr, bench_external_decay(data, fbsl, rr));
+                    log.debug("SSH_EXT {} {} {} {}", size, rr, bench_native_decay(data, ssh, rr), compute_are(trueHHs, ssh.getCounts()));
+                    log.debug("SSL_EXT {} {} {} {}", size, rr, bench_native_decay(data, ssl, rr), compute_are(trueHHs,
+                                                                                                          ssl.getCounts()));
+                    log.debug("FBSL_EXT {} {} {} {}", size, rr, bench_native_decay(data, fbsl, rr), compute_are(trueHHs, fbsl.getCounts()));
                 }
             }
         }
-
 
         return Arrays.asList(new AnalysisResult(0, 0, 0, 0, 0, new ArrayList<>()));
     }
