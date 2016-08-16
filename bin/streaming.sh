d145bb40c8801ac9c1a30146906def52b4ec2b6e
Peter Bailis <pbailis@gmail.com>
2016-08-15 22:17:36 -0700

    Fix bin/ classpath

--- bin/streaming.sh
+++ bin/streaming.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+conf_file=${1:-"conf/streaming.yaml"}
 
-java ${JAVA_OPTS} -cp "assembly/target/*:core/target/*:frontend/target/*:contrib/target/*" macrobase.MacroBase pipeline conf/streaming.yaml
+set -x
+java ${JAVA_OPTS} -cp "core/target/classes/*:frontend/target/classes/*:frontend/src/main/resources/:contrib/target/classes/*:assembly/target/*" macrobase.MacroBase pipeline $conf_file
