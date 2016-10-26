a6242a732b1d602cb9a62501b3343505e349ffcb
Arsen Mamikonyan <arsen@mamikonyan.am>
2016-10-26 01:27:30 -0400

    Improve documentation for python scripts

--- tools/helpers.py
+++ tools/helpers.py
@@ -1,4 +1,7 @@
 import functools
+import os
+import errno
+from time import strftime
 
 
 def retry_if_fails(max_retries):
@@ -22,3 +25,22 @@ def retry_if_fails(max_retries):
 
 
 retry_3_times = retry_if_fails(3)
+
+
+def makedirs_p(lowest_directory):
+  try:
+    os.makedirs(lowest_directory)
+  except Exception as e:
+    if e.errno != errno.EEXIST:
+      raise e
+
+
+def exp_bench_dir(workload):
+  sub_dir = os.path.join(
+    os.getcwd(),
+    'bench',
+    'workflows',
+    workload['macrobase.query.name'],
+    strftime('%m-%d-%H_%M_%S'))
+  makedirs_p(sub_dir)
+  return sub_dir
