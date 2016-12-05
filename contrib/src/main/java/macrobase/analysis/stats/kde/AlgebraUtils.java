4f8825378adb7ae713e35c22288ce4a6ac1e71a1
Edward Gan <edgan8@gmail.com>
2016-12-04 22:55:33 -0800

    Removing some dead code

--- contrib/src/main/java/macrobase/analysis/stats/kde/AlgebraUtils.java
+++ contrib/src/main/java/macrobase/analysis/stats/kde/AlgebraUtils.java
@@ -23,49 +23,4 @@ public class AlgebraUtils {
         }
         return boundaries;
     }
-
-    /**
-     * @param box1 box[d][0,1] are boundaries along dimension d
-     * @param box2
-     * @return two row vectors
-     */
-    public static double[][] getMinMaxDistanceBetweenBoxes(double[][] box1, double[][] box2) {
-        int k = box1.length;
-        double[][] minMaxDiff = new double[2][k];
-
-        for (int i=0; i<k; i++) {
-            double d1 = box2[i][0] - box1[i][1];
-            double d2 = box1[i][0] - box2[i][1];
-            // box2 to left
-            if (d2 >= 0) {
-                minMaxDiff[0][i] = d2;
-                minMaxDiff[1][i] = -d1;
-            }
-            // box2 to right
-            else if (d1 >= 0) {
-                minMaxDiff[0][i] = d1;
-                minMaxDiff[1][i] = -d2;
-            }
-            // inside
-            else {
-                d1 = FastMath.abs(d1);
-                d2 = FastMath.abs(d2);
-                minMaxDiff[0][i] = 0;
-                minMaxDiff[1][i] = d1 > d2 ? d1 : d2;
-            }
-        }
-
-        return minMaxDiff;
-    }
-
-    public static String array2dToString(double[][] data) {
-        StringBuilder s = new StringBuilder();
-        s.append("[");
-        for (double[] curRow : data) {
-            s.append(Arrays.toString(curRow));
-            s.append(",");
-        }
-        s.append("]");
-        return s.toString().trim();
-    }
 }
