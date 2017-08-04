1eccca78905f3495022f8a3b76acde379dc2070f
Edward Gan <edgan8@gmail.com>
2017-08-04 02:09:35 -0700

    starting code for grouper

--- lib/src/main/java/edu/stanford/futuredata/macrobase/analysis/summary/apriori/ExplanationResultGrouper.java
+++ lib/src/main/java/edu/stanford/futuredata/macrobase/analysis/summary/apriori/ExplanationResultGrouper.java
@@ -1,21 +1,50 @@
 package edu.stanford.futuredata.macrobase.analysis.summary.apriori;
 
-import edu.stanford.futuredata.macrobase.analysis.summary.Explanation;
 import edu.stanford.futuredata.macrobase.datamodel.DataFrame;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class ExplanationResultGrouper {
-    private Explanation explanation;
+    private APExplanation explanation;
     private DataFrame data;
 
     public ExplanationResultGrouper(
-            Explanation e,
+            APExplanation e,
             DataFrame d
     ) {
         this.explanation = e;
         this.data = d;
     }
 
-    public void group() {
+    public ArrayList<Integer> getMatches(Map<String, String> matcher) {
+        int d = matcher.size();
+        List<String> colNames = new ArrayList<>(matcher.keySet());
 
+        int n = data.getNumRows();
+        ArrayList<Integer> indices = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            indices.add(i);
+        }
+        for (int c = 0; c < d; c++) {
+            String colName = colNames.get(c);
+            String[] curColumn = data.getStringColumnByName(colName);
+            String matchValue = matcher.get(colName);
+            ArrayList<Integer> newIndices = new ArrayList<>(indices.size());
+            for (int i : indices) {
+                if (curColumn[i].equals(matchValue)) {
+                    newIndices.add(i);
+                }
+            }
+            indices = newIndices;
+        }
+
+        return indices;
+    }
+
+    public void group() {
+        ArrayList<ExplanationResult> results = explanation.getResults();
+        ArrayList<ArrayList<Integer>> resultMatches = new ArrayList<>(results.size());
     }
 }
