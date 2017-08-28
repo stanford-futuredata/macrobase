76c2131f4abfcbf23bcf308ea9b05caa7a7ff370
Edward Gan <edgan8@gmail.com>
2017-08-27 18:03:32 -0700

    hooking up rest of aria cube pipeline

--- core/src/main/java/edu/stanford/futuredata/macrobase/contrib/aria/AriaCubePipeline.java
+++ core/src/main/java/edu/stanford/futuredata/macrobase/contrib/aria/AriaCubePipeline.java
@@ -1,12 +1,17 @@
 package edu.stanford.futuredata.macrobase.contrib.aria;
 
+import edu.stanford.futuredata.macrobase.analysis.classify.ArithmeticClassifier;
+import edu.stanford.futuredata.macrobase.analysis.classify.CubeClassifier;
 import edu.stanford.futuredata.macrobase.analysis.summary.Explanation;
+import edu.stanford.futuredata.macrobase.analysis.summary.apriori.APrioriSummarizer;
 import edu.stanford.futuredata.macrobase.datamodel.DataFrame;
 import edu.stanford.futuredata.macrobase.pipeline.Pipeline;
 import edu.stanford.futuredata.macrobase.pipeline.PipelineConfig;
+import edu.stanford.futuredata.macrobase.util.MacrobaseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -68,8 +73,59 @@ public class AriaCubePipeline implements Pipeline {
         log.info("{} rows", df.getNumRows());
         log.info("Attributes: {}", attributes);
 
-        System.out.println(df);
-        return null;
+        startTime = System.currentTimeMillis();
+        CubeClassifier classifier = getClassifier();
+        classifier.process(df);
+        elapsed = System.currentTimeMillis() - startTime;
+        log.info("Classification time: {}", elapsed);
+        log.info("Outlier cutoffs: {} {}",
+                classifier.getLowCutoff(),
+                classifier.getHighCutoff()
+        );
+        df = classifier.getResults();
+
+        APrioriSummarizer summarizer = new APrioriSummarizer();
+        summarizer.setOutlierColumn(classifier.getOutputColumnName());
+        summarizer.setCountColumn(classifier.getCountColumnName());
+        summarizer.setAttributes(attributes);
+        summarizer.setMinSupport(minSupport);
+        summarizer.setMinRatioMetric(minRatioMetric);
+        startTime = System.currentTimeMillis();
+        summarizer.process(df);
+        elapsed = System.currentTimeMillis() - startTime;
+        log.info("Summarization time: {}", elapsed);
+        Explanation output = summarizer.getResults();
+        return output;
     }
 
+
+    public CubeClassifier getClassifier() throws MacrobaseException {
+        switch (classifierType) {
+            case "arithmetic": {
+                ArithmeticClassifier classifier =
+                        new ArithmeticClassifier(
+                                "Count",
+                                "Average",
+                                "StandardDeviation"
+                        );
+                classifier.setPercentile(cutoff);
+                classifier.setIncludeHigh(includeHi);
+                classifier.setIncludeLow(includeLo);
+                return classifier;
+            }
+            case "percentile": {
+                Map<String, Double> percentileColumns = new HashMap<>();
+                percentileColumns.put("Percentile001", 0.1);
+                percentileColumns.put("Percentile01", 1.0);
+                percentileColumns.put("Percentile05", 5.0);
+                percentileColumns.put("Percentile50", 50.0);
+                percentileColumns.put("Percentile95", 95.0);
+                percentileColumns.put("Percentile99", 99.0);
+                percentileColumns.put("Percentile999", 99.9);
+                return null;
+            }
+            default:
+                throw new MacrobaseException("Bad Classifier Name");
+        }
+    }
 }
