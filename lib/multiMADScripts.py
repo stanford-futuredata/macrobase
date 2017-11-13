05a6b4b9ced9e7851e2011f1ccc7b4c48c1484e1
Sahaana <sahaana.suri@gmail.com>
2017-11-12 23:43:22 -0800

    blah

--- lib/multiMADScripts.py
+++ lib/multiMADScripts.py
@@ -3,25 +3,25 @@ import subprocess
 import sys
 
 proc = "mvn test -Dtest=macrobase.analysis.classify.MultiMADOptimizationTest -Dinput={} -Dmetrics={} -Dattr={} -Dtrials={} -Ddataset={}"
-trials = 2
+trials = sys.argv[1]
 
-if sys.argv[1] == "cmt_sm":
+if sys.argv[2] == "cmt_sm":
     #CMT Test Query
     input = "/data/pbailis/preagg/cmt_sm.csv"
     metrics = "data_count_accel_samples,data_count_netloc_samples"
-    attr = "build_version"
+    attr = "state"
 
-elif sys.argv[1] == "cmt":
+elif sys.argv[2] == "cmt":
     input = "/data/pbailis/preagg/cmt.csv"
     metrics = "data_count_minutes,data_count_accel_samples,data_count_netloc_samples,data_count_gps_samples,distance_mapmatched_km,distance_gps_km,battery_drain_rate_per_hour"
-    attr = "build_version"
+    attr = "state"
 
-elif sys.argv[1] == "campaign_sm"
+elif sys.argv[2] == "campaign_sm":
     input = "/data/pbailis/preagg/campaign_2M.csv"
     metrics = "sms_in_activity,sms_out_activity,call_in_activity,call_out_activity,internet_traffic_activity"
     attr = "square_id"
 
-elif sys.argv[1] == "campaign"
+elif sys.argv[2] == "campaign":
     input = "/data/pbailis/preagg/campaign.csv"
     metrics = "sms_in_activity,sms_out_activity,call_in_activity,call_out_activity,internet_traffic_activity"
     attr = "square_id"
@@ -31,5 +31,5 @@ else:
     metrics = "A0,A1,A2,A3,A4,A5,A6,A7,A8"
     attr = "A9"
 
-subprocess.call(proc.format(input, metrics, attr, trials, sys.argv[1]), shell=True)
+subprocess.call(proc.format(input, metrics, attr, trials, sys.argv[2]), shell=True)
 
