42c75e82b846e1a55d8a7370ecf3aa9a84be2d42
Edward Gan <edgan8@gmail.com>
2017-12-07 11:46:49 -0800

    Updating conf files and accept all certificates

--- core/demo/query.sh
+++ core/demo/query.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 curl -H "Accept: application/json" -H "Content-type: application/json" \
--X POST -d "@core/demo/demo_query.json" 0.0.0.0:4567/query; echo
+-X POST -d "@core/demo/batch.json" 0.0.0.0:4567/query; echo
