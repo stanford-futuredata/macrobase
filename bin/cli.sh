2fa7ac959be42ebf56a771facd012493250b2e98
Peter Kraft <petereliaskraft@gmail.com>
2018-01-10 14:10:57 -0800

    remove accidently committed file

--- bin/cli.sh
+++ bin/cli.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/usr/bin/env bash
 BIN=`dirname "$0"`
 BASE=$BIN/../core
-java -Xmx6g  -agentpath:"C:\Program Files\YourKit Java Profiler 2017.02-b68\bin\win64\yjpagent.dll" -cp "$BASE/target/classes;$BASE/target/*" \
+java -Xmx4g -cp "$BASE/target/classes:$BASE/target/*" \
 edu.stanford.futuredata.macrobase.cli.CliRunner "$@"
