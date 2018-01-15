33c4aadc98871162a28dd579a9e49904db1c1281
Firas Abuzaid <fabuzaid21@gmail.com>
2018-01-14 20:49:26 -0800

    more rebasing fixes---importing CSVs was incorrect due to switch to Univocity

--- lib/src/main/java/edu/stanford/futuredata/macrobase/util/MacrobaseSQLException.java
+++ lib/src/main/java/edu/stanford/futuredata/macrobase/util/MacrobaseSQLException.java
@@ -2,7 +2,7 @@ package edu.stanford.futuredata.macrobase.util;
 
 public class MacrobaseSQLException extends MacrobaseException {
 
-  public MacrobaseSQLException(String message) {
-    super(message);
-  }
+    public MacrobaseSQLException(String message) {
+        super(message);
+    }
 }
