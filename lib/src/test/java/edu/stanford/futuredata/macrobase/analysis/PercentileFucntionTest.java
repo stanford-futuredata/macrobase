0c3a7c7de920e78be3346992d8e9ee1824e32818
Firas Abuzaid <fabuzaid21@gmail.com>
2018-01-23 13:10:11 -0800

    PercentileTest

---
+++ lib/src/test/java/edu/stanford/futuredata/macrobase/analysis/PercentileFucntionTest.java
@@ -0,0 +1,30 @@
+package edu.stanford.futuredata.macrobase.analysis;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import edu.stanford.futuredata.macrobase.util.MacrobaseException;
+import java.util.Arrays;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PercentileFucntionTest {
+
+    private double[] input;
+    private double[] expectedOutput;
+
+    @Before
+    public void setUp() throws Exception {
+        input = new double[]{0.1, 0.3, 0.2, 0.5, 0.4};
+        expectedOutput = new double[]{0.2, 0.6, 0.4, 1.0, 0.8};
+    }
+
+    @Test
+    public void simpleTest() throws MacrobaseException {
+        MBFunction func = MBFunction.getFunction("percentile", "");
+        final double[] output = new double[input.length];
+        func.applyFunction(input, output);
+        assertTrue(Arrays.equals(output, expectedOutput));
+    }
+
+}
