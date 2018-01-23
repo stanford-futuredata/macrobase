11fc00ee64460f18ad01523137caaf7090fa4651
Firas Abuzaid <fabuzaid21@gmail.com>
2018-01-23 00:18:13 -0800

    ON *

--- sql/demo.sql
+++ sql/demo.sql
@@ -1,4 +1,5 @@
-IMPORT FROM CSV FILE 'core/demo/sample.csv' INTO sample(usage double, latency double, location string, version string);
+IMPORT FROM CSV FILE 'core/demo/sample.csv' INTO sample(usage double, latency
+  double, location string, version string);
 
 SELECT * FROM
   DIFF
@@ -11,8 +12,23 @@ SELECT * FROM
   DIFF
     (SELECT * FROM sample WHERE usage > 1000.0) outliers,
     (SELECT * FROM sample WHERE usage < 1000.0) inliers
+  ON *;
+
+SELECT * FROM
+  DIFF
+    (SELECT * FROM sample WHERE usage > 1000.0) outliers,
+    (SELECT * FROM sample WHERE usage < 1000.0) inliers
   ON
     location, version
+  WITH MIN RATIO 5.0 MIN SUPPORT 0.75
+  COMPARE BY
+    risk_ratio(COUNT(*));
+
+SELECT * FROM
+  DIFF
+    (SELECT * FROM sample WHERE usage > 1000.0) outliers,
+    (SELECT * FROM sample WHERE usage < 1000.0) inliers
+  ON *
   WITH MIN RATIO 5.0 MIN SUPPORT 0.75
   COMPARE BY
     risk_ratio(COUNT(*));
