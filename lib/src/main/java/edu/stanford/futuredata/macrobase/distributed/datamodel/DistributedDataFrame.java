ca8bf683ca24c654daeb32698177a648e39f5a92
Peter Kraft <petereliaskraft@gmail.com>
2018-02-22 15:11:23 -0800

    more functional distributed data frame

--- lib/src/main/java/edu/stanford/futuredata/macrobase/distributed/datamodel/DistributedDataFrame.java
+++ lib/src/main/java/edu/stanford/futuredata/macrobase/distributed/datamodel/DistributedDataFrame.java
@@ -3,13 +3,34 @@ package edu.stanford.futuredata.macrobase.distributed.datamodel;
 import edu.stanford.futuredata.macrobase.datamodel.Schema;
 import org.apache.spark.api.java.JavaPairRDD;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class DistributedDataFrame {
-    public Schema schema;
+    private Schema schema;
     public JavaPairRDD<String[], double[]> dataFrameRDD;
+    private Map<String, Integer> nameToIndexMap;
 
     public DistributedDataFrame(Schema schema, JavaPairRDD<String[], double[]> dataFrameRDD) {
         this.schema = schema;
         this.dataFrameRDD = dataFrameRDD;
+
+        nameToIndexMap = new HashMap<>();
+        List<String> doubleNames = schema.getColumnNamesByType(Schema.ColType.DOUBLE);
+        List<String> stringNames = schema.getColumnNamesByType(Schema.ColType.STRING);
+        for (int i = 0; i < doubleNames.size(); i++)
+            nameToIndexMap.put(doubleNames.get(i), i);
+        for (int i = 0; i < stringNames.size(); i++)
+            nameToIndexMap.put(stringNames.get(i), i);
+    }
+
+    public Schema.ColType getTypeOfColumn(String colName) {
+        return schema.getColumnTypeByName(colName);
+    }
+
+    public int getIndexOfColumn(String colName) {
+        return nameToIndexMap.get(colName);
     }
 
 }
