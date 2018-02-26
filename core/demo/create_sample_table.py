6b43e4d38e12ecda27742ec1a198e5bc03da0648
Eric Xu <ericxu0@stanford.edu>
2018-02-25 22:41:31 -0800

    testing synthetic datasets

--- core/demo/create_sample_table.py
+++ core/demo/create_sample_table.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 import random
 
-NUMROWS = 100000
-NUMCOLS = 100
+NUMROWS = 1000000
+NUMCOLS = 8
 cols = [str('A' + str(i)) for i in range(NUMCOLS)]
 
 usage = np.random.rand(NUMROWS)
@@ -11,7 +11,7 @@ data = []
 for i in range(NUMROWS):
     vals = [random.random()]
     for j in range(NUMCOLS):
-        vals.append(np.random.choice(5, p=[0.7, 0.1, 0.1, 0.05, 0.05]))
+        vals.append(np.random.choice(1000))
     data.append(tuple(vals))
 
 df = pd.DataFrame(data=data, columns=['usage'] + cols)
