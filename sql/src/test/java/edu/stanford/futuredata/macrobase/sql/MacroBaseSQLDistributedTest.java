8de421fcc0ea967564943b485087432570a140f7
Peter Kraft <petereliaskraft@gmail.com>
2018-02-28 17:17:41 -0800

    Fixed parsing bugs, diff tests started but need explanation->dataset<row> function to be complete

--- sql/src/test/java/edu/stanford/futuredata/macrobase/sql/MacroBaseSQLDistributedTest.java
+++ sql/src/test/java/edu/stanford/futuredata/macrobase/sql/MacroBaseSQLDistributedTest.java
@@ -2,7 +2,9 @@ package edu.stanford.futuredata.macrobase.sql;
 
 import edu.stanford.futuredata.macrobase.sql.parser.ParsingException;
 import edu.stanford.futuredata.macrobase.sql.parser.SqlParser;
+import edu.stanford.futuredata.macrobase.sql.tree.DiffQuerySpecification;
 import edu.stanford.futuredata.macrobase.sql.tree.ImportCsv;
+import edu.stanford.futuredata.macrobase.sql.tree.Query;
 import edu.stanford.futuredata.macrobase.sql.tree.Statement;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
@@ -52,6 +54,45 @@ public class MacroBaseSQLDistributedTest {
         assertEquals("4.0", collectedResult.get(2).getString(0));
         assertEquals("28", collectedResult.get(2).getString(1));
         assertEquals("USA", collectedResult.get(2).getString(2));
+
+        spark.stop();
+    }
+
+    @Test
+    public void testDiffBasic() throws Exception {
+        final String importStr = "IMPORT FROM CSV FILE \"../core/demo/sample.csv\" INTO sample(usage string, latency string, location string, version string);";
+        SqlParser parser = new SqlParser();
+        SparkSession spark = SparkSession
+                .builder()
+                .master("local[4]")
+                .appName("macrobase-sql-spark")
+                .getOrCreate();
+        QueryEngineDistributed queryEngineDistributed = new QueryEngineDistributed(spark);
+        final Statement importStmt;
+        try {
+            importStmt = parser.createStatement(importStr.replace(";", ""));
+            assertTrue( "ingestQuery should generate a Statement of type Query",
+                    importStmt instanceof ImportCsv);
+        } catch (ParsingException e) {
+            e.printStackTrace();
+            throw new Error("ingestQuery should parse");
+        }
+        final ImportCsv importStatement = (ImportCsv) importStmt;
+        queryEngineDistributed.importTableFromCsv(importStatement);
+
+        final String diffStr = "SELECT * FROM DIFF (SELECT * FROM sample WHERE version=\"v1\") outliers, (SELECT * FROM sample WHERE version=\"v2\") inliers ON location WITH MIN RATIO 1.2 MIN SUPPORT 0.05;";
+
+        final Statement diffStmt;
+        try {
+            diffStmt = parser.createStatement(diffStr.replace(";", ""));
+        } catch (ParsingException e) {
+            e.printStackTrace();
+            throw new Error("ingestQuery should parse");
+        }
+        final Query diffQuery = (Query) diffStmt;
+        Dataset<Row> diffResult = queryEngineDistributed.executeQuery(diffQuery);
+
+        assertEquals(279, diffResult.count());
 
         spark.stop();
     }
