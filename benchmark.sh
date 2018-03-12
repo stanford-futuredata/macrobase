aebb070d72987c566187622b8d13a88e4898adee
ericxu0 <ericxu0@dawn1.stanford.edu>
2018-03-11 22:52:47 -0700

    fixed issues in parsing

--- benchmark.sh
+++ benchmark.sh
@@ -4,8 +4,8 @@ set -x
 
 git checkout firas/func-dependencies-master
 ./build.sh core
-bin/cli.sh conf/fed_disbursements.json > synthetic-func-dependencies-fed_disbursements.txt
+bin/cli.sh conf/fed_disbursements.json > func-dependencies-fed_disbursements.txt
 
 git checkout firas/scratch
 ./build.sh core
-bin/cli.sh conf/fed_disbursements.json > synthetic-master-funcs-fed_disbursements.txt
+bin/cli.sh conf/fed_disbursements.json > master-fed_disbursements.txt
