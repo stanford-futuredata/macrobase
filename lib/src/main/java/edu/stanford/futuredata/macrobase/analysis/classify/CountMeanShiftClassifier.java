9c5b3ffe185a89ea49302b7bcf6e7a905ae4da98
Peter Kraft <petereliaskraft@gmail.com>
2018-05-09 12:53:22 -0700

    Integrating into BasicBatchPipeline

--- lib/src/main/java/edu/stanford/futuredata/macrobase/analysis/classify/CountMeanShiftClassifier.java
+++ lib/src/main/java/edu/stanford/futuredata/macrobase/analysis/classify/CountMeanShiftClassifier.java
@@ -7,34 +7,32 @@ import edu.stanford.futuredata.macrobase.util.MacroBaseException;
 import java.util.function.DoublePredicate;
 import java.util.function.Predicate;
 
-public class CountMeanShiftCubedClassifier extends CubeClassifier {
+public class CountMeanShiftClassifier extends Classifier {
     private Predicate<String> strPredicate;
     private DoublePredicate doublePredicate;
     private DataFrame output;
     private String metricColumnName;
     private String meanColumnName;
     private boolean isStrPredicate;
-    public static String outlierCountColumnName = CountMeanShiftClassifier.outlierCountColumnName;
-    public static String inlierCountColumnName = CountMeanShiftClassifier.inlierCountColumnName;
-    public static String outlierMeanSumColumnName = CountMeanShiftClassifier.outlierMeanSumColumnName;
-    public static String inlierMeanSumColumnName = CountMeanShiftClassifier.inlierMeanSumColumnName;
+    public static String outlierCountColumnName = "_OUTLIERCOUNT";
+    public static String inlierCountColumnName = "_INLIERCOUNT";
+    public static String outlierMeanSumColumnName = "_OUTLIERMEANSUM";
+    public static String inlierMeanSumColumnName = "_INLIERMEANSUM";
 
     /**
-     * @param countColumnName Column containing per-row counts
      * @param metricColumnName Column on which to classify outliers
      * @param meanColumnName Column containing means whose shifts will be explained
      * @param predicateStr Predicate used for classification: "==" or "!="
      * @param sentinel String sentinel value used when evaluating the predicate to determine outlier
      * @throws MacroBaseException
      */
-    public CountMeanShiftCubedClassifier(
-            final String countColumnName,
+    public CountMeanShiftClassifier(
             final String metricColumnName,
             final String meanColumnName,
             final String predicateStr,
             final String sentinel
     ) throws MacroBaseException {
-        super(countColumnName);
+        super(meanColumnName);
         this.metricColumnName = metricColumnName;
         this.meanColumnName = meanColumnName;
         this.strPredicate = MBPredicate.getStrPredicate(predicateStr, sentinel);
@@ -42,20 +40,18 @@ public class CountMeanShiftCubedClassifier extends CubeClassifier {
     }
 
     /**
-     * @param countColumnName Column containing per-row counts
      * @param metricColumnName Column on which to classify outliers
      * @param meanColumnName Column containing means whose shifts will be explained
      * @param predicateStr Predicate used for classification: "==", "!=", "<", ">", "<=", or ">="
      * @param sentinel Double sentinel value used when evaluating the predicate to determine outlier
      */
-    public CountMeanShiftCubedClassifier(
-            final String countColumnName,
+    public CountMeanShiftClassifier(
             final String metricColumnName,
             final String meanColumnName,
             final String predicateStr,
             final double sentinel
     ) throws MacroBaseException {
-        super(countColumnName);
+        super(meanColumnName);
         this.metricColumnName = metricColumnName;
         this.meanColumnName = meanColumnName;
         this.doublePredicate = MBPredicate.getDoublePredicate(predicateStr, sentinel);
@@ -70,30 +66,33 @@ public class CountMeanShiftCubedClassifier extends CubeClassifier {
      */
     @Override
     public void process(DataFrame input) throws Exception {
-        String[] stringMetrics = input.getStringColumnByName(metricColumnName);
-        double[] doubleMetrics = input.getDoubleColumnByName(metricColumnName);
+        String[] stringMetrics = null;
+        if (isStrPredicate)
+            stringMetrics = input.getStringColumnByName(metricColumnName);
+        double[] doubleMetrics = null;
+        if (!isStrPredicate)
+            doubleMetrics = input.getDoubleColumnByName(metricColumnName);
         output = input.copy();
-        double[] totalCountColumn = input.getDoubleColumnByName(getCountColumnName());
         double[] totalMeanColumn = input.getDoubleColumnByName(meanColumnName);
-        int len = totalCountColumn.length;
+        int len = totalMeanColumn.length;
         double[] outlierCountColumn = new double[len];
         double[] inlierCountColumn = new double[len];
-        double[] outlierMeanSumColumn = new double[len];
-        double[] inlierMeanSumColumn = new double[len];
+        double[] outlierMeanColumn = new double[len];
+        double[] inlierMeanColumn = new double[len];
         for (int i = 0; i < len; i++) {
-                if ((isStrPredicate && strPredicate.test(stringMetrics[i])) ||
-                        (!isStrPredicate && doublePredicate.test(doubleMetrics[i]))) {
-                    outlierCountColumn[i] = totalCountColumn[i];
-                    outlierMeanSumColumn[i] = totalMeanColumn[i] * totalCountColumn[i];
-                } else {
-                    inlierCountColumn[i] = totalCountColumn[i];
-                    inlierMeanSumColumn[i] = totalMeanColumn[i] * totalCountColumn[i];
-                }
+            if ((isStrPredicate && strPredicate.test(stringMetrics[i])) ||
+                    (!isStrPredicate && doublePredicate.test(doubleMetrics[i]))) {
+                outlierCountColumn[i] = 1.0;
+                outlierMeanColumn[i] = totalMeanColumn[i];
+            } else {
+                inlierCountColumn[i] = 1.0;
+                inlierMeanColumn[i] = totalMeanColumn[i];
+            }
         }
         output.addColumn(outlierCountColumnName, outlierCountColumn);
         output.addColumn(inlierCountColumnName, inlierCountColumn);
-        output.addColumn(outlierMeanSumColumnName, outlierMeanSumColumn);
-        output.addColumn(inlierMeanSumColumnName, inlierMeanSumColumn);
+        output.addColumn(outlierMeanSumColumnName, outlierMeanColumn);
+        output.addColumn(inlierMeanSumColumnName, inlierMeanColumn);
     }
 
 
