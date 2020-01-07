6a63a5908a2c8b1b657586b51385a8f982e7afcf
Firas Abuzaid <fabuzaid21@gmail.com>
2020-01-07 12:23:35 -0800

    nit

--- lib/src/main/java/edu/stanford/futuredata/macrobase/analysis/summary/aplinear/AntiDiffSubtractSummarizer.java
+++ lib/src/main/java/edu/stanford/futuredata/macrobase/analysis/summary/aplinear/AntiDiffSubtractSummarizer.java
@@ -13,7 +13,7 @@ import org.slf4j.LoggerFactory;
 
 public class AntiDiffSubtractSummarizer extends APLOutlierSummarizer {
 
-    private Logger log = LoggerFactory.getLogger("APLOutlierSummarizer");
+    private Logger log = LoggerFactory.getLogger("AntiDiffSubtractSummarizer");
 
     public AntiDiffSubtractSummarizer(boolean useBitmaps) {
         super(useBitmaps);
